d2fcb5ee46133504b0a744df4b6f7112eec2fddb
ramneetk0002 <ramneet0002@gmail.com>
2025-08-19 09:13:14 +0530

    Lecture-18

---
+++ lec18/model/blog.js
@@ -0,0 +1,26 @@
+const mongoose=require("mongoose")
+const Schema=mongoose.Schema
+const BlogPost=new Schema({
+    title:String,
+    body:String,
+    date:Date,
+    userId:{
+        type:mongoose.Types.ObjectId,
+        ref:"Users"
+    }
+});
+module.exports=mongoose.model('Blogs', BlogPost)
+
+
+// const mongoose=require("mongoose");
+// const Schema = mongoose.Schema;
+// const BlogPost = new Schema({
+//   title: String,
+//   body: String,
+//   date: Date,
+//   userId:{
+//          type:mongoose.Types.ObjectId,
+//          ref:"Users"
+//      }
+// });
+// module.exports=mongoose.model('Blogs', BlogPost)
